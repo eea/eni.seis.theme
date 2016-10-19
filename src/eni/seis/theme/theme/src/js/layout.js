5e15626368d931e0368bbfbb2d1e112c1491fd57
triplem1 <themacaneata@gmail.com>
2016-10-19 18:17:41 +0300

    new design for homepage, landing page, article - work in progress

---
+++ src/eni/seis/theme/theme/src/js/layout.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	  if ( $('table').hasClass('listing') ) {
+            $('table').removeClass('listing').addClass('table table-responsive table-hover');
+        }
+
+})
