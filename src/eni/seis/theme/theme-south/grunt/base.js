670066f3e8ad2ec4c5454779c64a18a54b86ab0e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-05-04 15:43:30 +0300

    Move templates to src

--- src/eni/seis/theme/theme-south/grunt/base.js
+++ src/eni/seis/theme/theme-south/grunt/base.js
@@ -2,15 +2,15 @@ module.exports = {
  template: {
     dev: {
       engine: 'handlebars',
-      cwd: 'mockup/',
-      partials: ['mockup/partials/*.hbs'],
+      cwd: 'src/tpl/',
+      partials: ['src/tpl/partials/*.hbs'],
       // data: 'test/fixtures/data/data.json',
       options: {
       },
       files: [
         {
           expand: true,     // Enable dynamic expansion.
-          cwd: 'mockup/',      // Src matches are relative to this path.
+          cwd: 'src/tpl/',      // Src matches are relative to this path.
           src: '*.hbs', // Actual pattern(s) to match.
           dest: './',   // Destination path prefix.
           ext: '.html'  // Dest filepaths will have this extension.
