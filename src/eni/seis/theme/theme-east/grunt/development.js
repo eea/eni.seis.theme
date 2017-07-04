709110f0fe02c040d4a54d8d3954d934541634b3
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-07-04 15:48:31 +0300

    [refs #86208] Add theme-east.

---
+++ src/eni/seis/theme/theme-east/grunt/development.js
@@ -0,0 +1,61 @@
+module.exports = {
+  less: {
+    development: {
+      options: {
+        compress: false,
+        sourceMap: true,
+        sourceMapFilename: '<%= path.static %>/css/source.css.map',
+        sourceMapURL: './source.css.map'
+      },
+      files: {
+        '<%= path.static %>/css/main.css': '<%= path.src %>/less/main.less',
+        '<%= path.static %>/css/east.css': '<%= path.src %>/less/east.less',
+        '<%= path.static %>/css/south.css': '<%= path.src %>/less/south.less'
+      }
+    }
+  },
+  concat: {
+    scripts: {
+      src: [
+        '<%= path.src %>/js/**/*.js'
+      ],
+      dest: '<%= path.static %>/js/main.js'
+    }
+  },
+  // copy: {
+  //   scripts: {
+  //     files: [
+  //       { expand: true,
+  //         cwd: '<%= path.src%>/js/',
+  //         src: [
+  //           'bootstrap.dropdown.js'
+  //         ],
+  //         dest: '<%= path.static %>/js/'
+  //       }
+  //     ]
+  //   }
+  // },
+  watch: {
+    styles: {
+      files: ['<%= path.src %>/less/**/*.less'],
+      tasks: ['less:development'],
+      options: {
+        nospawn: true
+      }
+    },
+    scripts: {
+      files: ['<%= path.src %>/js/**/*.js'],
+      tasks: ['concat'],
+      options: {
+        nospawn: true
+      }
+    },
+    templates: {
+      files: ['<%= path.static %>/src/tpl/**/*.hbs'],
+      tasks: ['template:dev'],
+      options: {
+        nospawn: true
+      }
+    }
+  }
+}
